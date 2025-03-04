9cfd90ac95dcddbc7f30bb3129da3b0f21c82a18
David Khugaev <ddkhugaev@gmail.com>
2025-03-04 17:03:52 +0300

    выполнил дз

---
+++ WebApplicationCourse/Controllers/CalculatorController.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace WebApplicationCourse.Controllers
+{
+    public class CalculatorController : Controller
+    {
+        //public IActionResult Index()
+        //{
+        //    return View();
+        //}
+
+        public string Index(float a, float b)
+        {
+            return $"{a} + {b} = {a + b}";
+        }
+    }
+}
