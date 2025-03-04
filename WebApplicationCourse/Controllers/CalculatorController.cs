98b31e6a2c117aeb1ac7eebf4c5d5fce9f96a441
David Khugaev <ddkhugaev@gmail.com>
2025-03-04 17:40:57 +0300

    выполнил дз

---
+++ WebApplicationCourse/Controllers/CalculatorController.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace WebApplicationCourse.Controllers
+{
+    public class CalculatorController : Controller
+    {
+        //public IActionResult Index()
+        //{
+        //    return View();
+        //}
+
+        public string Index(float a, float b, string oper = "+")
+        {
+            switch (oper)
+            {
+                case "+": return $"{a} + {b} = {a + b}";
+                case "-": return $"{a} - {b} = {a - b}";
+                case "*": return $"{a} * {b} = {a * b}";
+                default: return "Возможные операции: '+', '-', '*'";
+            }
+        }
+    }
+}
