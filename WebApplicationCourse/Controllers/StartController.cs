1e3eddd7c5ee6a2af276cab5777021b59273b66f
David Khugaev <ddkhugaev@gmail.com>
2025-03-04 16:56:14 +0300

    выполнил дз

---
+++ WebApplicationCourse/Controllers/StartController.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace WebApplicationCourse.Controllers
+{
+    public class StartController : Controller
+    {
+        //public IActionResult Index()
+        //{
+        //    return View();
+        //}
+
+        public string Hello()
+        {
+            int hour = DateTime.Now.Hour;
+            if (hour < 6)
+            {
+                return "Доброй ночи";
+            }
+            if (hour < 12)
+            {
+                return "Доброе утро";
+            }
+            if (hour < 18)
+            {
+                return "Добрый день";
+            }
+            return "Добрый вечер";
+        }
+    }
+}
