d4b742eee87da6cc30c9c253c662cb68eec61d12
David Khugaev <ddkhugaev@gmail.com>
2025-03-25 16:12:17 +0300

    добавлены модели корзины и позиции корзины

---
+++ WebApplicationCourse/Models/Cart.cs
@@ -0,0 +1,18 @@
+﻿namespace WebApplicationCourse.Models
+{
+    public class Cart
+    {
+        public List<PositionCart> PositionsCart = new List<PositionCart>();
+        public decimal Cost { get; set; }
+        public void AddPositionToCart(PositionCart positionCart)
+        {
+            PositionsCart.Add(positionCart);
+            Cost += positionCart.Total;
+        }
+        public void RemovePositionFromCart(PositionCart positionCart)
+        {
+            PositionsCart.Remove(positionCart);
+            Cost -= positionCart.Total;
+        }
+    }
+}
---
+++ WebApplicationCourse/Models/PositionCart.cs
@@ -0,0 +1,25 @@
+﻿namespace WebApplicationCourse.Models
+{
+    public class PositionCart
+    {
+        public Product Prod { get; set; }
+        public int Count { get; set; }
+        public decimal Total { get; set; }
+        public PositionCart(Product prod, int count)
+        {
+            Prod = prod;
+            Count = count;
+            Total = Prod.Cost * count;
+        }
+        public void IncreaseCount(int count)
+        {
+            Count += count;
+            Total = Prod.Cost * Count;
+        }
+        public void DecreaseCount(int count)
+        {
+            Count -= count;
+            Total = Prod.Cost * Count;
+        }
+    }
+}
