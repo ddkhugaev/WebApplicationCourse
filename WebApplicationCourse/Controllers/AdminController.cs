cc43fe2c8daa35bfff535f11d28517a21fa94cab
David Khugaev <ddkhugaev@gmail.com>
2025-04-06 17:29:02 +0300

    Добавлена страница админа

---
+++ WebApplicationCourse/Controllers/AdminController.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace WebApplicationCourse.Controllers
+{
+    public class AdminController : Controller
+    {
+        public IActionResult Index()
+        {
+            return RedirectToAction("Orders");
+        }
+        public IActionResult Orders()
+        {
+            return View();
+        }
+        public IActionResult Users()
+        {
+            return View();
+        }
+        public IActionResult Roles()
+        {
+            return View();
+        }
+        public IActionResult Products()
+        {
+            return View();
+        }
+    }
+}
