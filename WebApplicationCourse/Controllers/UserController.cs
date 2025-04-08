e5513e4f9299482486618900db869b5a7a78c353
David Khugaev <ddkhugaev@gmail.com>
2025-04-08 14:54:25 +0300

    Валидация регистрации и авторизации пользователя

--- WebApplicationCourse/Controllers/UserController.cs
+++ WebApplicationCourse/Controllers/UserController.cs
@@ -1,4 +1,5 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using WebApplicationCourse.Models;
 
 namespace WebApplicationCourse.Controllers
 {
@@ -12,9 +13,32 @@ namespace WebApplicationCourse.Controllers
         {
             return View();
         }
+        [HttpPost]
+        public IActionResult Index(User user)
+        {
+            ModelState.Remove("ConfirmPassword");
+            if (ModelState.IsValid)
+            {
+                return Content(user.ToString());
+            }
+            return View(user);
+        }
         public IActionResult Registration()
         {
             return View();
+        }
+        [HttpPost]
+        public IActionResult Registration(User user)
+        {
+            if (user.Login == user.Password)
+            {
+                ModelState.AddModelError("", "Логин и пароль не должны совпадать");
+            }
+            if (ModelState.IsValid)
+            {
+                return Content(user.ToString());
+            }
+            return View(user);
         }
     }
 }
---
+++ WebApplicationCourse/Models/User.cs
@@ -0,0 +1,25 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace WebApplicationCourse.Models
+{
+    public class User
+    {
+        public Guid Id { get; set; }
+
+        [Required(ErrorMessage = "Не указан логин")]
+        [StringLength(25, MinimumLength = 2, ErrorMessage = "Длина логина должна быть от 2 до 25 символов")]
+        public string Login { get; set; }
+
+        [Required(ErrorMessage = "Не указан пароль")]
+        public string Password { get; set; }
+
+        [Required(ErrorMessage = "Не указан пароль повторно")]
+        [Compare("Password", ErrorMessage = "Пароли не совпадают")]
+        public string ConfirmPassword { get; set; }
+
+        public override string ToString()
+        {
+            return $"{Login} {Password}";
+        }
+    }
+}
